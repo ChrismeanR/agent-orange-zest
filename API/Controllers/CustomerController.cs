cb6ad710d319ea5db6de13f2ca42f1d8edf1a3f0
Christine Romano <chrismean@gmail.com>
2020-01-14 17:21:38 -0700

    Add context for getting agents customers, add new model to accommodate fields, update controller to reflect changes

--- API/Controllers/CustomerController.cs
+++ API/Controllers/CustomerController.cs
@@ -21,17 +21,14 @@ namespace API.Controllers
         public IEnumerable<AgentOrange.Models.Customer> Get()
         {
             gobjListCustomer = CustomerContext.GetCustomerData();
-            
             return gobjListCustomer;
         }
 
-        // GET: api/Customer/5
-        [HttpGet("{id}", Name = "GetCustomerById")]
-        public Customer Get(int id)
+        // GET: api/Customer/5054
+        [HttpGet("{id}", Name = "GetCustomersByAgentId")]
+        public IList<AgentCustomers> Get(int id)
         {
-            gobjListCustomer = CustomerContext.GetCustomerData();
-            var customer = gobjListCustomer.Select(item => item).Where(x=> x.Id ==id).FirstOrDefault();
-            return customer;
+            return CustomerContext.GetCustomersByAgent(id);
         }
 
         // POST: api/Customer
